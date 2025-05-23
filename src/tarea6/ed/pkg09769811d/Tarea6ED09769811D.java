9686a12e5626aaeeb543623df7ff8e94de0adf5a
magarciaperez <magarciaperez@gmail.com>
2025-05-23 13:31:46 +0200

    Iniciamosla primera version

---
+++ src/tarea6/ed/pkg09769811d/Tarea6ED09769811D.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea6.ed.pkg09769811d;
+
+/**
+ *
+ * @author migarci3
+ */
+public class Tarea6ED09769811D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tarea6EDClase miCarrito = new Tarea6EDClase();
+        miCarrito.aplicarDescuento(100, 5);
+    }
+    
+}
---
+++ src/tarea6/ed/pkg09769811d/Tarea6EDClase.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea6.ed.pkg09769811d;
+
+/**
+ *
+ * @author migarci3
+ */
+import java.util.logging.Logger;
+
+public class Tarea6EDClase {
+    public void aplicarDescuento(double precioProducto, int numProductos) {
+        double Total;
+        if (numProductos > 3)
+            precioProducto -= DESCUENTO_POR_CANTIDAD;
+
+        if (numProductos > 0) {
+            Total = precioProducto * DESCUENTO_GENERAL;
+            imprimirTotal(Total);
+        } else {
+            Total = precioProducto * DESCUENTO_POCOS_PRODUCTOS;
+            imprimirTotal(Total);
+        }
+    }
+    private static final double DESCUENTO_POCOS_PRODUCTOS = 0.95;
+    private static final double DESCUENTO_GENERAL = 0.8;
+    private static final int DESCUENTO_POR_CANTIDAD = 5;
+
+    private void imprimirTotal(double Total) {
+        System.out.println("El total a pagar es:" + Total);
+        System.out.println("Enviado");
+    }
+}
+
+
